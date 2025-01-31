6c48a58f71f690df76909c8cafdb52f81e6edf1f
snehasastry0310@utexas.edu <snehasastry0310@utexas.edu>
2025-01-31 23:09:44 +0000

    Edited Login

--- client_code/Login/__init__.py
+++ client_code/Login/__init__.py
@@ -10,6 +10,10 @@ class Login(LoginTemplate):
 
     # Any code you write here will run before the form opens.
 
-  def pwd_entered_pressed_enter(self, **event_args):
-    """This method is called when the user presses Enter in this text box"""
-    pass
+  def outlined_button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    mail = self.mail_entered.text
+    pwd = self.pwd_entered.text
+    # Call your 'send_feedback' server function
+    # pass in name, email and feedback as arguments
+    anvil.server.call('send_feedback', mail, pwd)
