0c46391344e3412f2ee36d787f18ab172426dac1
snehasastry0310@utexas.edu <snehasastry0310@utexas.edu>
2025-01-31 15:35:01 +0000

    Edited Login

--- client_code/Login/__init__.py
+++ client_code/Login/__init__.py
@@ -9,3 +9,7 @@ class Login(LoginTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def pwd_entered_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    pass
