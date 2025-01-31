8e0085951eec1e72dd28d3ad2e1e6da111400652
snehasastry0310@utexas.edu <snehasastry0310@utexas.edu>
2025-01-31 14:49:06 +0000

    Created Login; Edited settings; Deleted Form1

--- client_code/Login/__init__.py
+++ client_code/Login/__init__.py
@@ -1,9 +1,9 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import LoginTemplate
 from anvil import *
 import anvil.server
 
 
-class Form1(Form1Template):
+class Login(LoginTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
