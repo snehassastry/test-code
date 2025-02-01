d90e48ac52b40d927e54373655583f5e9efd4abd
snehasastry0310@utexas.edu <snehasastry0310@utexas.edu>
2025-02-01 20:05:34 +0000

    Created LICENSE.txt; Edited Login and Blog

--- client_code/Blog/__init__.py
+++ client_code/Blog/__init__.py
@@ -6,28 +6,15 @@ from anvil.tables import app_tables
 import anvil.users
 import anvil.server
 from anvil import open_form
-def initiate_login(self):
-    anvil.users.login_with_form()
+anvil.users.login_with_form()
 
 class Blog(BlogTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
-    # Any code you write here will run before the form opens.
-
-  def outlined_button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    mail = self.mail_entered.text
-    pwd = self.pwd_entered.text
-    # Call your 'send_feedback' server function
-    # pass in name, email and feedback as arguments
-    anvil.server.call("send_feedback", mail, pwd)
-
   def home_click(self, **event_args):
     """This method is called when the button is clicked"""
     open_form('Login')
-
   def blog_click(self, **event_args):
     """This method is called when the button is clicked"""
     open_form('Blog')
--- client_code/Login/__init__.py
+++ client_code/Login/__init__.py
@@ -11,14 +11,9 @@ class Login(LoginTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    
-    # Any code you write here will run before the form opens.
-
-
   def button_1_click(self, **event_args):
     """Navigate to Form2 when button is clicked"""
     open_form('Blog')  
-
   def home_click(self, **event_args):
     """This method is called when the button is clicked"""
     open_form('Login') 
